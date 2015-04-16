4c646ee98bff3468979853ff2aa6b5bfb42f86a6
Kai Pak <kai@kaipak.org>
2015-04-15 22:31:08 -0700

    wtf?

--- wavesolve.py
+++ wavesolve.py
@@ -3,17 +3,6 @@
   Wavesolve program main program.  Runs main routine and calls calculations 
   from functions from math and physics libraries.
   
-  @required_packages
-  bigfloat - Python wrapper for MPFR for handling arbitrarily large precision 
-             floating point numbers.
-  MPFR - GNU C library for multiple-precision floating-point computation.  Note,
-         GMP is required for installation.
-         http://www.mpfr.org
-  GMP - GNU Multiple Precision library for arbitrary precision 
-        arithmetic, operating on signed integers, rational numbers, and 
-        floating-point numbers.
-        https://gmplib.org/
-  
   @author Kai Pak
   @start_date January 1, 2014
   @current_version 1.0
@@ -26,14 +15,14 @@
 import itertools
 import timeit
 import multiprocessing as mp
-from numpy import matrix, linalg, pi, set_printoptions
+from numpy import array, matrix, linalg, pi, set_printoptions
 from IPython.display import display
 
 # Custom libraries
 import ws_maths
 import ws_physics
 
-NSIZE = 20
+NSIZE = 4
 Z = 1
 PREC = 16
 
@@ -47,7 +36,7 @@ def main():
     wave_equations = ws_physics.make_waves(4)
     psis = []
     
-    """
+    
     # Chris Wave funcs
     psis.append(ws_physics.gen_wavefunction(0, 0, 0))
     psis.append(ws_physics.gen_wavefunction(0, 0, 1))
@@ -61,19 +50,24 @@ def main():
     for i in xrange(0, NSIZE):
         psis.append(wave_equations[i])
         i += 1
-    
+    """ 
    
-    matrix_ij = [[0.0] * NSIZE for i in xrange(NSIZE)] 
+    matrix_ij = [[0.0] * NSIZE for i in xrange(NSIZE)]
+    matrix_ij = matrix_ij
+    print matrix_ij
     
-    # Build matrix in following manner:
-    # [ n11, n12, n13]
-    # [      n22, n23]
-    # [           n33]
-    #
-    # As this is a symmetric matrix (<psi_i|psi_j> <psi_j|psi_i>, we save roughly n/2
-    # processing time by just mirroring from upper triangular
-    # Multiprocessing version
-    #
+    """
+    Build matrix in following manner:
+    [ n11, n12, n13]
+    [      n22, n23]
+    [           n33]
+   
+    As this is a symmetric matrix (<psi_i|psi_j> <psi_j|psi_i>, we save roughly n/2
+    processing time by just mirroring from upper triangular
+    
+    Multiprocessing version
+    
+    """
     for i in xrange(0, NSIZE):
         print 'Constructing row', i, 'of <i|j>'
         pool = mp.Pool(processes=None)
@@ -81,12 +75,23 @@ def main():
                   for j in xrange(0, NSIZE)]
         psirow = [p.get() for p in psirowobj]
         matrix_ij[i] = psirow
-    print matrix_ij
+        
+        """
+        # Now populate row and it's transpose column since this is a symmetric matrix
+        for j in xrange(0, len(psirow)):
+            matrix_ij[i][i+j] = psirow[j]
+            matrix_ij[i+j][i] = psirow[j]
+        """    
+        
+    for row in matrix_ij:
+        print row
+        
     matrix_ij_time = timeit.default_timer()
     print "\n\nTime elapsed in seconds: "
     print matrix_ij_time - start_time
     
     # Now build <Psi_i|H|Psi_j> over matrix in similar manner as previous step
+    # Multiprocessor version
     hamiltonians = [] # Where we'll store wave equations with applied H operator
     for i in psis:
         hamiltonians.append(ws_physics.hamiltonian_r(i, Z))
@@ -98,10 +103,12 @@ def main():
         print 'Constructing row', i, 'of <i|H|j>'
         pool = mp.Pool(processes=None)
         psi_iHj_rowobj = [pool.apply_async(ws_physics.get_qstate, args=(psis[i],\
-                          hamiltonians[j])) for j in xrange(0, NSIZE)]
+                          hamiltonians[j])) for j in xrange(i, NSIZE)]
         psi_iHj_row = [p.get() for p in psi_iHj_rowobj]
         matrix_iHj[i] = psi_iHj_row
             
+    print matrix_iHj
+    
     matrix_iHj_time = timeit.default_timer()
     print "\n\nTime elapsed in seconds: "
     print matrix_iHj_time - start_time
